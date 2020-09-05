cf56eec2dfb97946a85cf8ad01e8b7b2f009d642
seyunpark <50330914+seyunpark@users.noreply.github.com>
2020-09-06 00:38:39 +0900

    signup picture2

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,7 +19,7 @@ class SignupForm(UserCreationForm):
     }))
 
     nickname = forms.CharField(label='닉네임')
-    picture = forms.ImageField(label='프로필 사진', required=False)
+    picture = forms.ImageField(label='프로필 사진', required=False, blank=True, null=True)
 
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
